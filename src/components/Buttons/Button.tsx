c08d618022d2a3f1c22323ff78bdbd1510f015a1
SammyBits <samuelbeato7@gmail.com>
2024-08-09 20:16:59 -0400

    feat: Add react-icons package for using icons in Button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ShowComponent } from "../Utils/ShowComponent";
 interface props {
   title: string;
-  mode: "primary" | "secondary";
+  mode: "primary" | "secondary" | "none";
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
   onClick?: () => void;
@@ -19,13 +19,15 @@ export const Button = ({
   const modeStyle =
     mode === "primary"
       ? "bg-emerald-500 text-white border"
-      : "bg-transparent text-neutral-900 border";
+      : "text-neutral-900 border";
 
   return (
     <button onClick={onClick} className={`${modeStyle} ${styleExtras}`}>
-      <ShowComponent isShowing={iconPosition === "left"}>{icon}</ShowComponent>
+    <div className="flex items-center">
+    <ShowComponent isShowing={iconPosition === "left"}><span className="mr-2">{icon}</span></ShowComponent>
       {title}
-      <ShowComponent isShowing={iconPosition === "right"}>{icon}</ShowComponent>
+      <ShowComponent isShowing={iconPosition === "right"}><span className="mr-2">{icon}</span></ShowComponent>
+    </div>
     </button>
   );
 };
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,4 +1,8 @@
+import { MdArrowRightAlt } from "react-icons/md";
+import { Button } from "../components/Buttons/Button";
 import { Hero } from "../components/Commons/Hero";
+import { CiLocationArrow1 } from "react-icons/ci";
+import { FaRegPlayCircle } from "react-icons/fa";
 
 function IndexPage() {
   return (
@@ -9,13 +13,14 @@ function IndexPage() {
             Nuvix Dev: Donde los desarrolladores se encuentran y colaboran
           </p>
 
-          <p className="pt-4 font-lato font-normal text-lg text-center  w-[516px] text-neutral-600">
+          <p className="pt-4 font-lato font-normal text-lg text-center w-[516px] text-neutral-600">
             "Nuvix reúne a profesionales y aficionados de la tecnología en un
             solo lugar. Conéctate, comparte tus ideas y encuentra nuevas
             oportunidades. Con Nuvix Dev, podrás conocer a personas afines y
             construir amistades basadas en intereses tecnológicos comunes."
           </p>
         </div>
+        
         <div className="absolute top-[380px] left-[200px] bottom-0 flex items-center">
           <img
             src="/public/images/landingPage/first-agent.png"
@@ -23,11 +28,28 @@ function IndexPage() {
             className="w-[302px] h-[444px]"
           />
         </div>
-        <div className="absolute top-[380px] right-[200px] bottom-0 flex items-center">
+        <div className="absolute top-[380px] right-[200px] bottom-0 flex items-center ">
           <img
             src="/public/images/landingPage/second-agent.png"
             alt="Right"
             className="w-[324px] h-[444px]"
+          />
+        </div>
+
+        <div className="absolute bottom-[20px] top-[100%] left-[49rem] transform -translate-x-1/2 flex gap-11">
+          <Button 
+            mode="none" 
+            title="Comenzar" 
+            icon={<MdArrowRightAlt className="text-2xl" />}
+            iconPosition="left" 
+            styleExtras="border border-none rounded rounded-[22px] bg-[#0EA89BFF] font-lato text-white h-[44px] px-[16px] flex items-center"
+          />
+          <Button 
+            mode="secondary" 
+            title="Ver demo"
+            icon={<FaRegPlayCircle className="text-xl" />}
+            iconPosition="left"
+            styleExtras="border border-[#0EA89BFF] rounded rounded-[22px] text-[#0EA89BFF] font-lato h-[44px] px-[18px] flex items-center"
           />
         </div>
       </Hero>
